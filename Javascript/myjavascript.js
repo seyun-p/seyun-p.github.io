5bd7e9fb24d54458279107cac9751156a51c2752
seyun-p <149702477+seyun-p@users.noreply.github.com>
2023-11-20 19:59:59 +0900

    javascript hw

---
+++ Javascript/myjavascript.js
@@ -0,0 +1,12 @@
+function show() {
+ 	  /* Get image and change value 
+      of src attribute */
+      let image = document.getElementById("mydiv100");
+      image.src ="images/wp6124202.JPG"
+      document.getElementById("btnID")
+      .style.display = "none";}
+			
+
+
+		
+	 
