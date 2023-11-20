694363397033a73a8d4994de058ebb207acb89f6
seyun-p <149702477+seyun-p@users.noreply.github.com>
2023-11-20 20:50:23 +0900

    javascript hw valodator checked

--- Javascript/myjavascript.js
+++ Javascript/myjavascript.js
@@ -1,12 +1,8 @@
-function show() {
- 	  /* Get image and change value 
-      of src attribute */
-      let image = document.getElementById("mydiv100");
-      image.src ="images/wp6124202.JPG"
-      document.getElementById("btnID")
-      .style.display = "none";}
-			
-
-
-		
-	 
+function show()
+{
+	/* Get image and change value of src attribute */
+	let image = document.getElementById("mydiv100");
+	image.src ="images/wp6124202.JPG"
+	document.getElementById("btnID")
+	.style.display = "none";
+}
